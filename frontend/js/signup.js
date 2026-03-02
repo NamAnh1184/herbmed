27fd4a44cbb57b7d4ea74be164ee8109b3ef4ab0
NamAnh1184 <nayeuhanh@gmail.com>
2026-03-02 16:52:45 +0700

    Update signup.js

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,6 +1,4 @@
-// =====================================================
-// SIGNUP.JS - Handle signup form submission
-// =====================================================
+
 
 // API Configuration
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
